309c839876694b17eef097a7bcd3e232747d1339
WillianAbrego <willianantonioabrego@gmail.com>
2023-12-19 02:54:11 -0600

    creacion de tablas, campos y forma en que estan relacionadas

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _ from "lodash";
 window._ = _;
 
 /**
@@ -7,10 +7,10 @@ window._ = _;
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -33,3 +33,16 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: "pusher",
+    key: "${PUSHER_APP_KEY}",
+    wsHost: window.location.hostname,
+    wsPort: import.meta.env.VITE_PUSHER_PORT,
+    forceTLS: false,
+    disableStats: true,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+});
